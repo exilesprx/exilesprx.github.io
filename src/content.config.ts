6e6330c5ad2fd9964282ef6e534b31edabcd8516
Exiles.prx <exiles.prx@gmail.com>
2025-05-03 14:07:25 -0400

    original copy over

---
+++ src/content.config.ts
@@ -0,0 +1,23 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+const blog = defineCollection({
+  loader: glob({ pattern: '**/*.md', base: './src/content/blog' }),
+  schema: z.object({
+    author: z.string().default('Andrew Campbell'),
+    title: z.string(),
+    description: z.string(),
+    slug: z.string().optional(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    tags: z.array(z.string()),
+    isDraft: z.boolean(),
+    image: z
+      .object({
+        src: z.string(),
+        alt: z.string()
+      })
+      .default({ src: '', alt: '' })
+  })
+});
+
+export const collections = { blog };
---
+++ src/content/blog/initial.md
@@ -0,0 +1,10 @@
+---
+title: My first post
+description: A post in the root directory of the blog
+slug: 'initial'
+pubDate: 1744837297694
+tags: ['general']
+isDraft: false
+---
+
+Your blog post content here.
