96acf8834acc9697707817001d0882fcabc64a2c
Exiles.prx <exiles.prx@gmail.com>
2026-05-05 17:07:56 -0400

    fix: prettier config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,5 +1,11 @@
 /** @type {import("prettier").Config} */
 export default {
+  semi: true,
+  tabWidth: 2,
+  singleQuote: true,
+  printWidth: 80,
+  trailingComma: 'none',
+  singleAttributePerLine: true,
   plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
   overrides: [
     {
