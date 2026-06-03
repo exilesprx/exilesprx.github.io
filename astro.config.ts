e79e6a60985e9045e99929aebac0876bcddc4554
Exiles.prx <exiles.prx@gmail.com>
2026-06-03 18:19:52 -0400

    fix: lint

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss() as any]
+    plugins: [tailwindcss()]
   },
 
   integrations: [icon()],
